acbd4f593e240fca1f9ac7943d1ab3ba26cc0b8a
Walifer <waliferkainan2008@gmail.com>
2026-07-14 13:18:47 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javanauta/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,11 @@
+package com.javanauta.usuario.infrastructure.repository;
+
+
+import com.javanauta.aprendendo_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
+
+}
