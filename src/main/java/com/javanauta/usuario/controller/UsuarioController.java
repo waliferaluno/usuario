0a0ef392e3f474c52cf1e87cbb799cb153606659
Walifer <waliferkainan2008@gmail.com>
2026-07-29 10:08:05 -0300

    Me ajude professora

--- src/main/java/com/javanauta/usuario/controller/UsuarioController.java
+++ src/main/java/com/javanauta/usuario/controller/UsuarioController.java
@@ -27,10 +27,16 @@ public class UsuarioController {
 
     @PostMapping("/login")
     public String login(@RequestBody UsuarioDTO usuarioDTO){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(), usuarioDTO.getSenha())
-        );
-        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            usuarioDTO.getEmail(), usuarioDTO.getSenha()));
+            return "Bearer " + jwtUtil.generateToken(authentication.getName());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @GetMapping
